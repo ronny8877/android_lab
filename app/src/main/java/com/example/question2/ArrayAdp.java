bad9d2ea136cdad9aed05d42e690517d04bd630b
Ronnie <sunnyrawat225@gmail.com>
2021-11-27 08:16:32 +0530

    i swear i hate college

--- app/src/main/java/com/example/question2/ArrayAdp.java
+++ app/src/main/java/com/example/question2/ArrayAdp.java
@@ -2,13 +2,26 @@ package com.example.question2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ArrayAdp extends AppCompatActivity {
-
+ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_array_adp);
+   listView = findViewById(R.id.list);
+
+   String[] arr ={"Ronnie","Is","a ","Good","Boi"};
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this, R.layout.activity_array_adp,arr);
+        listView.setAdapter(adapter);
+
+
+        SQLiteDatabase mydatabase = openOrCreateDatabase("your database name",MODE_PRIVATE,null);
+        mydatabase.execSQL("CREATE TABLE IF NOT EXISTS TutorialsPoint(Username VARCHAR,Password VARCHAR);");
+        mydatabase.execSQL("INSERT INTO TutorialsPoint VALUES('admin','admin');");
     }
 }
